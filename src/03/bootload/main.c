ea57b214a170e4eccdcd8102f41b2c74b5fd3464
bobchin <bobchin.ryu@gmail.com>
2015-08-31 16:17:23 +0900

    静的変数のコピー処理を追加

--- src/03/bootload/main.c
+++ src/03/bootload/main.c
@@ -2,6 +2,21 @@
 #include "serial.h"
 #include "lib.h"
 
+static int init(void)
+{
+    /* can use the symbol defined at "ld.scr"    */
+    extern int erodata, data_start, edata, bss_start, ebss;
+
+    /* initialize "data" and "bss" */
+    memcpy(&data_start, &erodata, (long)&edata - (long)&data_start);
+    memset(&bss_start, 0, (long)&ebss - (long)&bss_start);
+
+    /* initialize serial */
+    serial_init(SERIAL_DEFAULT_DEVICE);
+
+    return 0;
+}
+
 int main(void)
 {
     serial_init(SERIAL_DEFAULT_DEVICE);
