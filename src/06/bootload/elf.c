d32ed156b11abfb9c57715d1931549e3f5772d49
bobchin <bobchin.ryu@gmail.com>
2015-09-03 10:11:13 +0900

    セグメントをRAM上にコピーするようにした

--- src/06/bootload/elf.c
+++ src/06/bootload/elf.c
@@ -68,13 +68,8 @@ static int elf_load_program(struct elf_header *header)
 
     if (phdr->type != 1) continue;
 
-    putxval(phdr->offset,        6); puts(" ");
-    putxval(phdr->virtual_addr,  8); puts(" ");
-    putxval(phdr->physical_addr, 8); puts(" ");
-    putxval(phdr->file_size,     5); puts(" ");
-    putxval(phdr->memory_size,   5); puts(" ");
-    putxval(phdr->flags,         2); puts(" ");
-    putxval(phdr->align,         2); puts("\n");
+    memcpy((char *)phdr->physical_addr, (char *)header + phdr->offset, phdr->file_size);
+    memset((char *)phdr->physical_addr + phdr->file_size, 0, phdr->memory_size - phdr->file_size);
   }
 
   return 0;
