43a47bae5979c1f86aabd7d424ba8a7ee2dc7422
bobchin <bobchin.ryu@gmail.com>
2015-09-07 11:02:30 +0900

    レディキューの操作

---
+++ src/08/os/kozos.c
@@ -0,0 +1,77 @@
+#include "defines.h"
+#include "kozos.h"
+#include "intr.h"
+#include "interrupt.h"
+#include "syscall.h"
+#include "lib.h"
+
+#define  THREAD_NUM 6
+#define  THREAD_NAME_SIZE 15
+
+/* スレッドコンテキスト */
+typedef struct _kz_context {
+  uint32 sp;
+} kz_context;
+
+/* タスクコントロールブロック */
+typedef struct _kz_thread {
+  struct _kz_thread *next;
+  char name[THREAD_NAME_SIZE + 1];
+  char *stack;
+
+  struct {
+    kz_func_t func;
+    int argc;
+    char **argv;
+  } init;
+
+  struct {
+    kz_syscall_type_t type;
+    kz_syscall_param_t *param;
+  } syscall;
+
+  kz_context context;
+} kz_thread;
+
+/* スレッドのレディキュー */
+static struct {
+  kz_thread *head;
+  kz_thread *tail;
+} readyque;
+
+static kz_thread *current;
+static kz_thread threads[THREAD_NUM];
+static kz_handler_t handlers[SOFTVEC_TYPE_NUM];
+
+void dispatch(kz_context *context);
+
+/* カレントスレッドをレディキューから抜き出す */
+static int getcurrent(void)
+{
+  if (current == NULL)
+    return -1;
+
+  readyque.head = current->next;
+  if (readyque.head == NULL) {
+    readyque.tail = NULL;
+  }
+  current->next = NULL;
+
+  return 0;
+}
+
+static int putcurrent(void)
+{
+  if (current == NULL) {
+    return -1;
+  }
+
+  if (readyque.tail) {
+    readyque.tail->next = current;
+  } else {
+    readyque.head = current;
+  }
+  readyque.tail = current;
+
+  return 0;
+}
