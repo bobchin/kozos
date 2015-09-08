aada1fd15f3b984730db8184edefe21b82f51d68
bobchin <bobchin.ryu@gmail.com>
2015-09-08 10:04:45 +0900

    コメントを修正

--- src/07/bootload/interrupt.h
+++ src/07/bootload/interrupt.h
@@ -4,16 +4,29 @@
 extern char softvec;
 #define SOFTVEC_ADDR (&softvec)
 
-/* ソフトウェア割り込みベクタの種類 */
+/*
+ * ソフトウェア割り込みベクタの種類
+ *（わかりやすいようにsoftvec_type_t型とする）
+ */
 typedef short softvec_type_t;
 
-/* 割り込みハンドラの型の定義 */
+/*
+ * 割り込みハンドラの型の定義
+ * （割り込みハンドラ（関数）をsoftvec_handler_t型とする）
+ */
 typedef void (*softvec_handler_t)(softvec_type_t type, unsigned long sp);
 
 /* ソフトウェア割り込みベクタの位置 */
 #define SOFTVECS ((softvec_handler_t *)SOFTVEC_ADDR)
 
-/* 割り込み有効化・無効化 */
+/*
+ * 割り込み有効化・無効化
+ * インラインアセンブラで記述している。
+ * やりたいことはCCR（コンディションコードレジスタ）の
+ * Iビット(7ビット)とUIビット(6ビット)を操作したい。
+ * 割り込み有効時は上位2ビットを0に、無効時は1にする。
+ * andc,orc はCPU命令
+ */
 #define INTR_ENABLE  asm volatile ("andc.b #0x3f,ccr")
 #define INTR_DISABLE asm volatile ("orc.b #0xc0,ccr")
 
