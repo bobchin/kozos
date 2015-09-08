40fc9a963249e697b3e46eb50b8599ebe219763b
bobchin <bobchin.ryu@gmail.com>
2015-09-08 10:04:11 +0900

    08を作成

--- src/08/os/kozos.c
+++ src/08/os/kozos.c
@@ -60,6 +60,7 @@ static int getcurrent(void)
   return 0;
 }
 
+/* カレントスレッドをレディキューに繋げる */
 static int putcurrent(void)
 {
   if (current == NULL) {
@@ -74,4 +75,195 @@ static int putcurrent(void)
   readyque.tail = current;
 
   return 0;
+}
+
+/* スレッドの終了 */
+static void thread_end(void)
+{
+  kz_exit();
+}
+
+/* スレッドのスタートアップ */
+static void thread_init(kz_thread *thp)
+{
+  thp->init.func(thp->init.argc, thp->init.argv);
+  thread_end();
+}
+
+/* システムコールの処理（kz_run():スレッドの起動） */
+static kz_thread_id_t thread_run(kz_func_t func, char *name, int stacksize, int argc, char *argv[])
+{
+  int i;
+  kz_thread *thp;
+  uint32 *sp;
+  extern char userstack;
+  static char *thread_stack = &userstack;
+
+  /* 開いているタスクコントロールブロックを検索 */
+  for (i = 0; i < THREAD_NUM; i++) {
+    thp = &threads[i];
+    if (!thp->init.func)
+      break;
+  }
+  if (i == THREAD_NUM)
+    return -1;
+
+  /* タスクコントロールブロックをゼロクリア */
+  memset(thp, 0, sizeof(*thp));
+
+  /* タスクコントロールブロックの設定 */
+  strcpy(thp->name, name);
+  thp->next = NULL;
+
+  thp->init.func = func;
+  thp->init.argc = argc;
+  thp->init.argv = argv;
+
+  /* スタック領域を獲得 */
+  memset(thread_stack, 0, stacksize);
+  thread_stack += stacksize;
+
+  thp->stack = thread_stack;
+
+  /* スタックの初期化 */
+  sp = (uint32 *)thp->stack;
+  *(--sp) = (uint32)thread_end;
+
+  /* プログラムカウンタを設定する */
+  *(--sp) = (uint32)thread_init;
+
+  *(--sp) = 0; /* ER6 */
+  *(--sp) = 0; /* ER5 */
+  *(--sp) = 0; /* ER4 */
+  *(--sp) = 0; /* ER3 */
+  *(--sp) = 0; /* ER2 */
+  *(--sp) = 0; /* ER1 */
+
+  /* スレッドのスタートアップ(thread_init())に渡す引数 */
+  *(--sp) = (uint32)thp; /* ER0 */
+
+  /* スレッドのコンテキストを設定 */
+  thp->context.sp = (uint32)sp;
+
+  /* システムコールを呼び出したスレッドをレディキューに戻す */
+  putcurrent();
+
+  /* 新規作成したスレッドをレディキューに接続する */
+  current = thp;
+  putcurrent();
+
+  return (kz_thread_id_t)current;
+}
+
+/* システムコールの処理(kz_exit():スレッドの終了) */
+static int thread_exit(void)
+{
+  puts(current->name);
+  puts(" EXIT.\n");
+  memset(current, 0, sizeof(*current));
+  return 0;
+}
+
+/* 割り込みハンドラの登録 */
+static int setintr(softvec_type_t type, kz_handler_t handler)
+{
+  static void thread_intr(softvec_type_t type, unsigned long sp);
+  softvec_setintr(type, thread_intr);
+
+  handlers[type] = handler;
+
+  return 0;
+}
+
+/* システムコールの呼び出し */
+static void call_functions(kz_syscall_type_t type, kz_syscall_param_t *p)
+{
+  switch (type) {
+    case KZ_SYSCALL_TYPE_RUN: /* kz_run() */
+      p->un.run.ret = thread_run(p->un.run.func, p->un.run.name, p->un.run.stacksize,
+                                 p->un.run.argc, p->un.run.argv);
+      break;
+
+    case KZ_SYSCALL_TYPE_EXIT: /* kz_exit() */
+      thread_exit();
+      break;
+
+    default:
+      break;
+  }
+}
+
+/* システムコールの処理 */
+static void syscall_proc(kz_syscall_type_t type, kz_syscall_param_t *p)
+{
+  getcurrent();
+  call_functions(type, p);
+}
+
+/* スレッドのスケジューリング */
+static void schedule(void)
+{
+  if (!readyque.head)
+    kz_sysdown();
+
+  current = readyque.head;
+}
+
+static void syscall_intr(void)
+{
+  syscall_proc(current->syscall.type, current->syscall.param);
+}
+
+static void softerr_intr(void)
+{
+  puts(current->name);
+  puts(" DOWN.\n");
+  getcurrent();
+  thread_exit();
+}
+
+static void thread_intr(softvec_type_t type, unsigned long sp)
+{
+  current->context.sp = sp;
+
+  if (handlers[type])
+    handlers[type]();
+
+  schedule();
+
+  dispatch(&current->context);
+
+  /* ここには返ってこない */
+}
+
+void kz_start(kz_func_t func, char *name, int stacksize, int argc, char *argv[])
+{
+  current = NULL;
+
+  readyque.head = readyque.tail = NULL;
+  memset(threads, 0, sizeof(threads));
+  memset(handlers, 0, sizeof(handlers));
+
+  setintr(SOFTVEC_TYPE_SYSCALL, syscall_intr);
+  setintr(SOFTVEC_TYPE_SOFTERR, softerr_intr);
+
+  current = (kz_thread *)thread_run(func, name, stacksize, argc, argv);
+
+  dispatch(&current->context);
+
+  /* ここには返ってこない */
+}
+
+void kz_sysdown(void)
+{
+  puts("system error!\n");
+  while (1)
+    ;
+}
+
+void kz_syscall(kz_syscall_type_t type, kz_syscall_param_t *param)
+{
+  current->syscall.type  = type;
+  current->syscall.param = param;
+  asm volatile ("trapa #0");
 }
